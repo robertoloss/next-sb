301a8a2781dcdbe7c6f6dcf9919e8a1076b77b13
Roberto Loss <robertoloss@gmail.com>
2025-01-11 00:13:47 -0500

    home page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,7 +3,7 @@
 export default async function Page() {
 
   return (
-    <div className="flex flex-row w-full h-full bg-red-300">
+    <div className="flex flex-row w-full h-full bg-red-800">
     </div>
   )
 }
