a32cb6b7fa9afea274cd39895d0f0c79f82afd81
Roberto Loss <robertoloss@gmail.com>
2025-01-23 13:18:01 -0500

    responsiveness fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -27,14 +27,11 @@ export default async function SideBar({ children }: { children: React.ReactNode}
       />
       <SidebarInset>
         <main className="h-full flex flex-col items-center w-full overflow-hidden">
-          <div className="flex-1 w-full flex flex-col items-center h-full">
+          <div className="flex-1 w-full flex flex-col items-center h-full overflow-hidden">
             <nav className="w-full flex items-center justify-center bg-sidebar-background pl-4 border-b h-16 border-b-muted-foreground z-40">
               <SidebarTrigger className="-ml-1 md:hidden" />
-              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                <div className="flex gap-5 items-center font-semibold">
-                  <div className="flex items-center gap-2">
-                  </div>
-                </div>
+              <div className="w-full flex flex-row justify-between items-center p-3 px-5 text-sm">
+                <div/>
                 <HeaderAuth/>
               </div>
             </nav>
--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -51,7 +51,7 @@ export default async function AuthButton() {
   }
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      <span className="hidden sm:flex">Hey, {user.email}!</span>
       <ThemeSwitcher/>
       <form action={signOutAction}>
         <Button type="submit" variant={"outline"}>
